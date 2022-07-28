f0a5dc9ec62aa2595fbac3d290e726cfb77ba856
jsumbak <julsumba311@gmail.com>
2022-07-28 10:29:47 -0700

    changing files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,10 +2,9 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// TODO: Add a comment describing the functionality of the withAuth middleware
-// connecting with auth from the other files to this route
+
 router.get('/', withAuth, async (req, res) => {
-  try { // we'll find all the user values but exclude password
+  try { 
     const userData = await User.findAll({
       attributes: { exclude: ['password'] },
       order: [['name', 'ASC']],
@@ -15,8 +14,7 @@ router.get('/', withAuth, async (req, res) => {
 
     res.render('homepage', {
       users,
-      // TODO: Add a comment describing the functionality of this property
-      // user will be prompted to login ot logout depending on their state
+  
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -25,8 +23,7 @@ router.get('/', withAuth, async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-  // TODO: Add a comment describing the functionality of this if statement
-  // once user is logged in they will be redirected to next page
+ 
   if (req.session.logged_in) {
     res.redirect('/');
     return;
