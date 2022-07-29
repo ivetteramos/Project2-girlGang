a6504598a8850c2abaca2f308261e36127273839
justnoserena <justnoserena@gmail.com>
2022-07-29 11:02:25 -0700

    Fixed Home route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,44 +3,15 @@ const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
 
-router.get('/', withAuth, async (req, res) => {
-  try { 
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      // Eventually, your application will qery into the recipe API and get some form of response
-      // This will probably be an arrya of objects where each object is a recipe
-      // you'll pass that array here in this object
-      recipes: [
-        {
-          id: 1,
-          name: "Chicken Alfredo",
-          ingredients: [
-            "Chicken",
-            "Alfredo"
-          ]
-        }
-      ],
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.get('/', (req, res) =>{
+  res.render('homepage');
 });
 
 router.get('/login', (req, res) => {
- 
   if (req.session.logged_in) {
     res.redirect('/');
     return;
   }
-
   res.render('login');
 });
 
