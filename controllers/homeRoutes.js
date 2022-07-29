50fc0719ea4d551568f40cf17cee048f31d14b70
ivetteramos <101986174+ivetteramos@users.noreply.github.com>
2022-07-29 13:56:17 -0700

    pushing file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-
 router.get('/', withAuth, async (req, res) => {
   try { 
     const userData = await User.findAll({
