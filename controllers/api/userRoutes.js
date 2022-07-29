336a7a613acad549dff558d38f313b09a67af4b4
jsumbak <julsumba311@gmail.com>
2022-07-29 14:48:15 -0700

    pushing current edits

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,13 +3,14 @@ const { User } = require('../../models');
 
 router.post('/login', async (req, res) => {
   try {
-  
+    console.log(req.body)
     const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res
         .status(400)
         .json({ message: 'Incorrect email or password, please try again' });
+        
       return;
     }
 
