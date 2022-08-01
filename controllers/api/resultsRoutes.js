75d55cadb7050d49354b923fb1c0ebc87a26e2f4
justnoserena <justnoserena@gmail.com>
2022-08-01 11:31:01 -0700

    Added functional buttons, favorite recipe route in homeRoutes.js

--- controllers/api/resultsRoutes.js
+++ controllers/api/resultsRoutes.js
@@ -7,8 +7,24 @@
 // verify they logged in with Auth
 // Then render results page
 
-// router.get("/results", withAuth, async (req,res) => {
-//try {
+// router.get('/results', withAuth, async (req, res) => {
+//     try {
+//     const userData = await User.findAll({
+//         attributes: { exclude: ['password'] },
+//         model: {[]},
+//     });
+
+//     const users = userData.map((project) => project.get({ plain: true }));
+
+//     res.render('results', {
+//         users,
+//         // Pass the logged in flag to the template
+//         logged_in: req.session.logged_in,
+//     });
+//     } catch (err) {
+//     res.status(500).json(err);
+//     }
+// });
 
 //}
 //});
